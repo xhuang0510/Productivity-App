75a106fcf22e2e0a1b16d03981bc6fd0cbfda758
Xinjie <xhuang2@uw.edu>
2020-12-29 19:00:43 -0600

    Reset
    Start new project

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -7,69 +7,13 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      keyword: "",
-      page: 0,
-      results: []
+      
     }
   }
 
-  // Update the state with current search box query
-  updateSearch = (textbox) => {
-    this.setState({
-      keyword: textbox.target.value
-    });
-  }
-
-  // Re-render the page with new state
-  refresh = () => {
-    this.getAmazonData();
-    this.getGoogleData();
-    this.render();
-  }
-
-  // Update to previous page
-  prevPage = () => {
-    this.setState({
-      page: this.state.page - 1
-    });
-  }
-
-  // Update to next page
-  nextPage = () => {
-    this.setState({
-      page: this.state.page + 1
-    });
-  }
-  
-  // Parsing Amazon search data
-  getAmazonData() {
-    let url = "https://cors-anywhere.herokuapp.com/https://www.amazon.com/s?k=" + this.state.keyword + "&page=" + this.state.page;
-    console.log(url);
-    let html = fetch(url)
-    .then(function(data) {
-      return data.text()
-    })
-    .then(function(data) { // We need to parse the data here
-      let doc = new DOMParser().parseFromString(data, "text/html");
-    })
-    this.state.results.push(html);
-  }
-
-  // Parsing Google shopping search data
-  getGoogleData() {
-    let keywordString = this.state.keyword.replace("/\s/g", "+");
-    let pageIndex = this.state.page * 20;
-    let url = "https://cors-anywhere.herokuapp.com/https://www.google.com/search?q=" + keywordString + "&tbm=shop&start=" + pageIndex;
-    console.log(url);
-    let html = fetch(url)
-    .then(function(data) {
-      return data.text()
-    })
-    .then(function(data) { // We need to parse the data here
-      let doc = new DOMParser().parseFromString(data, "text/html");
-      console.log(doc);
-    })
-    this.state.results.push(html);
+  // test
+  sayhello = () => {
+    console.log("Hello World!");
   }
 
   // Resets display state to default if window is resized
@@ -87,17 +31,7 @@ export class App extends Component {
     console.log("Rendering...");
     return (
       <div className="App">
-        <main>
-          <input type="text" id="searchBox" onChange={this.updateSearch} value={this.state.keyword} />
-          <button type="button" id="searchButton" onClick={this.refresh}>Search!</button>
-          <div id="searchResults">
-            We need the data here: 
-          </div> 
-          <div>
-            <button type="button" id="prevPageButton" onClick={this.prevPage}>Prev</button>
-            <button type="button" id="nextPageButton" onClick={this.nextPage}>Next</button>
-          </div>
-        </main>
+        
       </div>
     );
   }
--- shopping/src/Components.js
+++ shopping/src/Components.js
@@ -1,10 +1,4 @@
 import React, { Component } from 'react'; //import React Component
 import "./index.css";
 
-export class Item extends Component {
 
-}
-
-export class ItemList extends Component {
-    
-}
